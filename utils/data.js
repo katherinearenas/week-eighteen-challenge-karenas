7b0b169cd6d9cc0e541f1958f8d795327f61c072
Katherine <89894177+katherinearenas@users.noreply.github.com>
2024-09-29 14:06:01 -0500

    added thought seed data

--- utils/data.js
+++ utils/data.js
@@ -1,4 +1,4 @@
-const names = [
+const usernames = [
   'Aaran',
   'Aaren',
   'Aarez',
@@ -74,40 +74,42 @@ const names = [
   'Parker',
 ];
 
-const appDescriptions = [
-  'Decision Tracker',
-  'Find My Phone',
-  'Learn Piano',
-  'Starbase Defender',
-  'Tower Defense',
-  'Monopoly Money Manager',
-  'Movie trailers',
-  'Hello world',
-  'Stupid Social Media App',
-  'Notes',
-  'Messages',
-  'Email',
-  'Compass',
-  'Firefox',
-  'Running app',
-  'Cooking app',
-  'Poker',
-  'Deliveries',
+const thoughtText = [
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+'Curabitur efficitur justo et nulla posuere, quis fringilla metus blandit.',
+'Praesent a lectus sed urna porttitor interdum.',
+'Proin accumsan neque ac tempus congue.',
+'Ut malesuada eros in purus convallis, sed placerat neque laoreet.',
+'Etiam lacinia felis vehicula arcu sodales, ut gravida enim egestas.',
+'In quis metus ac sem convallis ultrices.',
+'Etiam cursus eros efficitur mauris vehicula venenatis.',
+'Sed placerat quam ac consequat suscipit.',
+'Praesent eu ex condimentum, egestas neque eget, vehicula nulla.',
+'Phasellus rutrum massa ac lectus luctus, eget consectetur lectus ultricies.',
+'Curabitur molestie odio tincidunt pharetra scelerisque.',
+'Fusce tempor ligula ut arcu tristique volutpat.',
+'Pellentesque porta tortor id risus consequat, facilisis volutpat velit condimentum.',
+'Aenean commodo augue in velit imperdiet eleifend.',
+'Nulla cursus metus et tincidunt porta.',
+'Cras pretium libero et mollis pretium.',
+'Integer tempus arcu sit amet rutrum malesuada.',
+'Maecenas facilisis arcu in felis elementum, eget porttitor velit semper.',
+'Duis quis velit id velit efficitur vehicula.',
 ];
 
 // Get a random item given an array
 const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 // Gets a random full name
-const getRandomName = () =>
-  `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
+const getRandomUser = () =>
+  `${getRandomArrItem(usernames)}`;
 
 // Function to generate random assignments that we can add to student object.
-const getRandomAssignments = (int) => {
+const getRandomThoughts = (int) => {
   const results = [];
   for (let i = 0; i < int; i++) {
     results.push({
-      assignmentName: getRandomArrItem(appDescriptions),
+      thoughtText: getRandomArrItem(thoughtText),
       score: Math.floor(Math.random() * (99 - 70 + 1) + 70),
     });
   }
@@ -115,4 +117,4 @@ const getRandomAssignments = (int) => {
 };
 
 // Export the functions for use in seed.js
-module.exports = { getRandomName, getRandomAssignments };
+module.exports = { getRandomUser, getRandomThoughts };
